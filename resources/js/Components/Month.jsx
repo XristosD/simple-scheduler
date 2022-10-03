d1bb3ac67984e44365279d6f6c420b999195e9ee
xristos <xristos567@gmail.com>
2022-10-03 12:30:48 +0200

    Refactoring Month component and dispaly month

--- resources/js/Components/Month.jsx
+++ resources/js/Components/Month.jsx
@@ -9,7 +9,8 @@ dayjs.extend(localeData);
 
 function Month({year, month}) {
 
-  const daysTextual = dayjs.weekdaysMin()
+  const daysText = dayjs.weekdaysMin();
+  const monthText = dayjs.months()[month-1];
 
   // TODO extract on helper function and move to parent
   const date = dayjs([year, month-1]);
@@ -26,16 +27,19 @@ function Month({year, month}) {
   }
 
   return (
-    <div>
-      <div className="grid grid-cols-7 auto-cols-max gap-1 p-1 border-2 rounded-md bg-slate-200 justify-items-center cursor-default drop-shadow">
-        {daysTextual.map(dayTextual => <div className="w-5 h-5 border-2 flex items-center justify-center rounded-md">
+    <div className='border-2 rounded-md bg-slate-200 cursor-default drop-shadow'>
+      <div className='text-center p-1 text-lg font-semibold text-indigo-900/90'>{monthText}</div>
+      <div className="grid grid-cols-7 auto-cols-max gap-1 p-1 justify-items-center">
+        {daysText.map(dayTextual => <div className="w-5 h-5 border-2 flex items-center justify-center rounded-md">
           <span className="text-xs font-semibold text-indigo-900/90">{dayTextual}</span>
           </div>
         )}
         {days.map(day => 
-          <div className="w-7 h-7 border-2 flex items-center justify-center rounded-md bg-white cursor-pointer">
-            <span className="text-xs font-semibold tabular-nums text-slate-600">{day}</span>
-          </div>
+          day != 0 ?
+          ( <div className="w-7 h-7 border-2 flex items-center justify-center rounded-md bg-white cursor-pointer hover:ring-1 ring-indigo-400 group">
+            <span className="text-xs font-semibold tabular-nums text-slate-600 group-hover:text-indigo-600 group-hover:font-black">{day}</span>
+          </div> )
+          : ( <div className="w-7 h-7 border-2 flex items-center justify-center rounded-md bg-slate-100/30"></div> )
         )}
       </div>
     </div>
