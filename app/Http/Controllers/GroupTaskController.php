8799e4a8ba24fb11a3424e25b09db0653af1f515
xristos <xristos567@gmail.com>
2022-10-12 15:31:13 +0200

    Update group function

--- app/Http/Controllers/GroupTaskController.php
+++ app/Http/Controllers/GroupTaskController.php
@@ -2,9 +2,17 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\GroupTask;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Redirect as Redirect;
+
 
 class GroupTaskController extends Controller
 {
-    //
+    public function update(Request $request, GroupTask $group)
+    {
+        $group->title = $request->title;
+        $group->save();
+        return Redirect::route('scheduler');
+    }
 }
