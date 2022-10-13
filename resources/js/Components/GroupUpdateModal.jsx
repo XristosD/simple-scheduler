91ebaa0acb3895d883192fd69513c73300fcd6b0
xristos <xristos567@gmail.com>
2022-10-13 10:54:05 +0200

    Refactoring update group modal behaviour and styles

--- resources/js/Components/GroupUpdateModal.jsx
+++ resources/js/Components/GroupUpdateModal.jsx
@@ -18,27 +18,28 @@ function GroupUpdateModal({ isOpen, setIsOpen, groupTitle, groupId }) {
   }
 
   return (
-      <Modal isOpen={isOpen} setOpenModal={setIsOpen} title={"Edit Group"}>
+      <Modal isOpen={isOpen} onClose={() => !processing && setIsOpen(false)} title={"Edit Group"}>
         <form onSubmit={submit} className='space-y-3'>
           <div className='flex flex-col'>
             <label htmlFor="title" className="text-sm text-indigo-500 font-medium pl-1 pb-0 peer-focus:text-indigo-900">Title</label>
             <input type="text" id="title" value={data.title} onChange={e => setData('title', e.target.value)} className="rounded-lg border-indigo-400 focus:border-indigo-500 focus:ring-indigo-500 peer"/>
           </div>
 
-          <div className="mt-4">
-            <button
-              type="button"
-              className="inline-flex justify-center rounded-md border border-transparent bg-blue-100 px-4 py-2 text-sm font-medium text-blue-900 hover:bg-blue-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-blue-500 focus-visible:ring-offset-2"
-              onClick={() => setIsOpen(false)}
-              >
-              cancel
-            </button>
+          <div className="pt-4 space-x-2">
             <button
               type="submit"
-              className="inline-flex justify-center rounded-md border border-transparent bg-blue-100 px-4 py-2 text-sm font-medium text-blue-900 hover:bg-blue-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-blue-500 focus-visible:ring-offset-2"
+              className="inline-flex justify-center rounded-md border border-transparent bg-indigo-200 px-4 py-2 text-sm font-medium text-indigo-900 hover:bg-indigo-300 focus:outline-none focus-visible:ring-2 focus-visible:ring-blue-500 focus-visible:ring-offset-2"
               disabled={processing}
               >
               save
+            </button>
+            <button
+              type="button"
+              className="inline-flex justify-center rounded-md border border-transparent bg-indigo-200 px-4 py-2 text-sm font-medium text-indigo-900 hover:bg-indigo-300 focus:outline-none focus-visible:ring-2 focus-visible:ring-blue-500 focus-visible:ring-offset-2"
+              onClick={() => setIsOpen(false)}
+              disabled={processing}
+              >
+              cancel
             </button>
           </div>
         </form>
