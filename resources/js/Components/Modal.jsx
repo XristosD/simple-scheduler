572d8c858e0aaaac24deace0de47febbbe3fc9cb
xristos <xristos567@gmail.com>
2022-10-13 10:54:45 +0200

    Modal tiltle styles and props rename

--- resources/js/Components/Modal.jsx
+++ resources/js/Components/Modal.jsx
@@ -1,10 +1,10 @@
 import React, { Children, Fragment } from 'react';
 import { Dialog, Transition } from '@headlessui/react';
 
-function Modal({isOpen, setOpenModal, title, children }) {
+function Modal({isOpen, onClose, title, children }) {
   return (
       <Transition appear show={isOpen} as={Fragment}>
-        <Dialog as="div" className="relative z-10" onClose={setOpenModal}>
+        <Dialog as="div" className="relative z-10" onClose={onClose}>
           <Transition.Child
             as={Fragment}
             enter="ease-out duration-300"
@@ -28,14 +28,18 @@ function Modal({isOpen, setOpenModal, title, children }) {
                 leaveFrom="opacity-100 scale-100"
                 leaveTo="opacity-0 scale-95"
               >
-                <Dialog.Panel className="w-full max-w-md transform overflow-hidden rounded-2xl bg-white p-6 text-left align-middle shadow-xl transition-all">
-                  <Dialog.Title
-                    as="h3"
-                    className="text-lg font-medium leading-6 text-gray-900"
-                  >
-                    { title }
-                  </Dialog.Title>
-                  { children }
+                <Dialog.Panel className="w-full max-w-md transform overflow-hidden rounded-2xl bg-white text-left align-middle shadow-xl transition-all">
+                  <div className='bg-indigo-200 p-6'>
+                    <Dialog.Title
+                      as="h3"
+                      className="text-lg font-medium leading-6 text-indigo-900"
+                    >
+                      { title }
+                    </Dialog.Title>
+                  </div>
+                  <div className='p-6'>
+                    { children }
+                  </div>
                 </Dialog.Panel>
               </Transition.Child>
             </div>
