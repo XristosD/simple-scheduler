0fcfe17afaf1b91599ce85bb4ebe279e77f477a5
xristos <xristos567@gmail.com>
2022-10-18 11:54:10 +0200

    Creates TaskUpdateModal component

--- resources/js/Components/Task.jsx
+++ resources/js/Components/Task.jsx
@@ -1,18 +1,12 @@
 import React,  { useState, Fragment } from 'react'
 import { FiEdit3 } from 'react-icons/fi';
-import Modal from '@/components/Modal';
+import TaskUpdateModal from '@/components/TaskUpdateModal';
 import Switcher from '@/Components/Switcher';
-import { useForm } from '@inertiajs/inertia-react';
 import dayjs from 'dayjs';
 
 function Task({ id, title, body, open, order, begin, end, groupId }) {
   const [itemOpen, setItemOpen] = useState(open);
-  const [openEditor, setOpenEditor] = useState(false);
-  const { data, setData, post, processing, errors } = useForm({
-    title: '',
-    body: '',
-    open: false,
-  })
+  const [openUpdateModal, setOpenUpdateModal] = useState(false);
 
   const setOpen = (open) => {
     setData('open', open);
@@ -25,36 +19,23 @@ function Task({ id, title, body, open, order, begin, end, groupId }) {
           <div>
             <span>{dayjs(begin).format('HH:mm')}</span> - <span>{dayjs(end).format('HH:mm')}</span>
           </div>
-          <button onClick={() => setOpenEditor(true)}><FiEdit3 className="text-sm cursor-pointer" /></button>
+          <button onClick={() => setOpenUpdateModal(true)}><FiEdit3 className="text-sm cursor-pointer" /></button>
         </div>
         <div className="text-md">{title}</div>
         <div className="flex justify-end text-xxs font-bold">
           <Switcher checked={itemOpen} setChecked={setItemOpen} />
         </div>
       </div>
-      <Modal isOpen={openEditor} setOpenModal={setOpenEditor} >
-        <form action="" className='space-y-3'>
-          <div className='flex flex-col'>
-            <label htmlFor="title" className="text-sm text-indigo-500 font-medium pl-1 pb-0 peer-focus:text-indigo-900">Title</label>
-            <input type="text" id="title" value={data.title} onChange={e => setData('title', e.target.value)} className="rounded-lg border-indigo-400 focus:border-indigo-500 focus:ring-indigo-500 peer"/>
-          </div>
-          <div className='flex flex-col'>
-            <label htmlFor="body" className="text-sm text-indigo-500 font-medium pl-1 pb-0 peer-focus:text-indigo-900">Body</label>
-            <textarea rows="3" id="body" value={data.body} onChange={e => setData('body', e.target.value)} className="rounded-lg border-indigo-400 focus:border-indigo-500 focus:ring-indigo-500 peer"/>
-          </div>
-          <Switcher checked={data.open} setChecked={setOpen} />
-        </form>
-
-        <div className="mt-4">
-          <button
-            type="button"
-            className="inline-flex justify-center rounded-md border border-transparent bg-blue-100 px-4 py-2 text-sm font-medium text-blue-900 hover:bg-blue-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-blue-500 focus-visible:ring-offset-2"
-            onClick={() => setOpenEditor(false)}
-          >
-            cancel
-          </button>
-        </div>
-      </Modal>
+      <TaskUpdateModal 
+        isOpen={openUpdateModal}
+        setIsOpen={setOpenUpdateModal}
+        id={id}
+        title={title}
+        body={body}
+        open={open}
+        begin={begin}
+        end={end}
+      />
     </>
   )
 }
---
+++ resources/js/Components/TaskUpdateModal.jsx
@@ -0,0 +1,64 @@
+import React from 'react';
+import Modal from '@/components/Modal';
+import Switcher from '@/Components/Switcher';
+import { useForm } from '@inertiajs/inertia-react';
+
+function TaskUpdateModal({ isOpen, setIsOpen, id, title, body, open, begin, end }) {
+
+  const { data, setData, put, processing, errors } = useForm({
+    id: id,
+    title: title,
+    body: body,
+    open: open,
+    begin: begin,
+    end: end,
+  })
+  
+
+  function submit(e) {
+    e.preventDefault()
+    put(`/task/${id}`, {
+      preserveScroll: true,
+      onSuccess: () => setIsOpen(false),
+    })
+  }
+
+  function setTaskOpen(val) {
+    setData('open', val)
+  } 
+
+  return (
+      <Modal isOpen={isOpen} onClose={() => !processing && setIsOpen(false)} title={"Edit Task"}>
+        <form onSubmit={submit} className='space-y-3'>
+          <div className='flex flex-col'>
+            <label htmlFor="title" className="text-sm text-indigo-500 font-medium pl-1 pb-0 peer-focus:text-indigo-900">Title</label>
+            <input type="text" id="title" value={data.title} onChange={e => setData('title', e.target.value)} className="rounded-lg border-indigo-400 focus:border-indigo-500 focus:ring-indigo-500 peer"/>
+          </div>
+          <div className='flex flex-col'>
+            <label htmlFor="body" className="text-sm text-indigo-500 font-medium pl-1 pb-0 peer-focus:text-indigo-900">Body</label>
+            <textarea rows="3" id="body" value={data.body} onChange={e => setData('body', e.target.value)} className="rounded-lg border-indigo-400 focus:border-indigo-500 focus:ring-indigo-500 peer"/>
+          </div>
+          <Switcher checked={data.open} setChecked={setTaskOpen} />
+          <div className="pt-4 space-x-2">
+            <button
+              type="submit"
+              className="inline-flex justify-center rounded-md border border-transparent bg-indigo-200 px-4 py-2 text-sm font-medium text-indigo-900 hover:bg-indigo-300 focus:outline-none focus-visible:ring-2 focus-visible:ring-blue-500 focus-visible:ring-offset-2"
+              disabled={processing}
+              >
+              save
+            </button>
+            <button
+              type="button"
+              className="inline-flex justify-center rounded-md border border-transparent bg-indigo-200 px-4 py-2 text-sm font-medium text-indigo-900 hover:bg-indigo-300 focus:outline-none focus-visible:ring-2 focus-visible:ring-blue-500 focus-visible:ring-offset-2"
+              onClick={() => setIsOpen(false)}
+              disabled={processing}
+              >
+              cancel
+            </button>
+          </div>
+        </form>
+      </Modal>
+  )
+}
+
+export default TaskUpdateModal
